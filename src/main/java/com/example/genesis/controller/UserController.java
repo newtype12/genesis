744baaf1a45129c3099b8ba3d7ca2c23066e8f60
leoho <newtype12@gmail.com>
2021-05-04 22:49:27 +0800

    基礎

---
+++ src/main/java/com/example/genesis/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.genesis.controller;
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/user")
+@Api(tags = "test")
+@Slf4j
+@RequiredArgsConstructor
+public class UserController {
+
+
+
+}
